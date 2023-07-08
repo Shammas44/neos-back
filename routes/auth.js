75370911214a1317507b495b2147817f89052206
Shammas44 <sebastien.traber@hotmail.com>
2023-07-08 16:47:39 +0200

    fix: wrong field name was provided for password

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 import MSG from '../utils/msg.js'
 import useAuth from '../utils/useAuth.js'
 import { send } from '../utils/common.js'
-import { setRow, TYPES as T } from '../utils/common.js'
 
 export function authRoute(router) {
   return (req, res, next) => {
@@ -9,10 +8,10 @@ export function authRoute(router) {
     const password = req.body?.password ?? ''
     if (!email && !password)
       return send(res, MSG.ERROR_CREDENTIALS_REQUIRED , 401)
-    const user = router.db.get("collaborators").find({ email, _password:password }).value()
+    const user = router.db.get("collaborators").find({ email, __password:password }).value()
     if (!user)
       return send(res, MSG.ERROR_CREDENTIALS_VALIDATION , 401)
-    if(user.email != email, user._password != password)
+    if(user.email != email, user.__password != password)
       return send(res, MSG.ERROR_CREDENTIALS_VALIDATION , 401)
     const token = useAuth.generateJwtToken(user.id, false)
     if (!token)
