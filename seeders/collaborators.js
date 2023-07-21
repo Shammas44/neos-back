e053b8f5d3725027acb41a5cdf48ebd1b19995ca
Shammas44 <sebastien.traber@hotmail.com>
2023-07-21 14:44:52 +0200

    refactor: complete refactor of the collaborators seeder

--- seeders/collaborators.js
+++ seeders/collaborators.js
@@ -1,41 +1,48 @@
 import faker from './config.js'
-import { ROLES_ARRAY, SITES_OBJECT } from '../data/common.js'
+import { ROLES_ARRAY, SITES_ARRAY, ROLES_OBJECT } from '../data/common.js'
 
 const MAX = 100
 const PRESENCE = ['présent', 'absent', 'congé']
 
 const collaborators = []
 
-for (let i = 0; i < MAX; i++) {
+function getCollaboratorFactory(firstName, lastName, optionalRole) {
+  return {
+    firstName: firstName || faker.person.firstName(),
+    lastName: lastName || faker.person.lastName(),
+    getCollaborator: function() {
+      const id = `${this.firstName[0]}${this.firstName[1]}${this.lastName[0]}${this.lastName[1]}`.toLowerCase()
+      const birthday = faker.date.past({ years: 40, refDate: "2003-01-01T00:00:00.000Z" }).getTime()
+      const role = optionalRole || ROLES_ARRAY[Math.floor(Math.random() * ROLES_ARRAY.length)]
+      const presence = PRESENCE[Math.floor(Math.random() * PRESENCE.length)]
+      const email = `${this.firstName}.${this.lastName}@orif.ch`
 
-  function getCollaborator() {
-    const id = `${this.firstName[0]}${this.firstName[1]}${this.lastName[0]}${this.lastName[1]}`.toLowerCase()
-    const birthday = faker.date.past({ years: 40,refDate: "2003-01-01T00:00:00.000Z" }).getTime()
-    const role = ROLES_ARRAY[Math.floor(Math.random() * ROLES_ARRAY.length)]
-    const presence = PRESENCE[Math.floor(Math.random() * PRESENCE.length)]
-    const email = `${this.firstName}.${this.lastName}@orif.ch`
-
-    return {
-      id: id,
-      firstName: this.firstName,
-      lastName: this.lastName,
-      phone: faker.phone.number('+41 ## ### ## ##'),
-      __password: id,
-      email: email,
-      birthday: String(birthday),
-      role: [role],
-      presence: [presence],
-      site: [SITES_OBJECT.MORGES],
+      return {
+        id: id,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        phone: faker.phone.number('+41 ## ### ## ##'),
+        __password: id,
+        email: email,
+        birthday: String(birthday),
+        role: [role],
+        presence: [presence],
+        site: [SITES_ARRAY[faker.number.int({ min: 0, max: SITES_ARRAY.length - 1 })][0]],
+      }
     }
   }
-
-  const collaboratorFactory = {
-    firstName: faker.person.firstName(),
-    lastName: faker.person.lastName(),
-    getCollaborator: getCollaborator,
-  }
-
-  collaborators.push(collaboratorFactory.getCollaborator())
 }
+
+for (let i = 0; i < MAX; i++) {
+  collaborators.push(getCollaboratorFactory().getCollaborator())
+}
+
+const users = [
+  ['Admin', 'Admin', ROLES_OBJECT.ADMIN]
+]
+
+users.forEach((user)=>{
+collaborators.push(getCollaboratorFactory(...user).getCollaborator())
+})
 
 export default collaborators
